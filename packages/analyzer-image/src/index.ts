574d553b88d2596126916b6b81759a1556e40c25
Justas Palumickas <jpalumickas@gmail.com>
2022-10-24 20:33:43 +0300

    Add image analyzer package

---
+++ packages/analyzer-image/src/index.ts
@@ -0,0 +1,36 @@
+import { Analyzer } from '@uplo/types';
+import sharp from 'sharp';
+
+const ORIENTATIONS = [
+  'Undefined',
+  'Top-Left',
+  'Top-Right',
+  'Bottom-Right',
+  'Bottom-Left',
+  'Left-Top',
+  'Right-Top',
+  'Right-Bottom',
+  'Left-Bottom',
+];
+
+const ImageAnalyzer =
+  (): Analyzer =>
+  async ({ blob: { contentType }, filePath }) => {
+    if (!contentType.startsWith('image/')) return;
+
+    const metadata = await sharp(filePath).metadata();
+    if (!metadata) return;
+    if (!metadata.orientation) return;
+
+    const orientation = ORIENTATIONS[metadata.orientation];
+    const isRotated = ['Right-Top', 'Left-Bottom'].includes(orientation);
+
+    const result = {
+      width: isRotated ? metadata.height : metadata.width,
+      height: isRotated ? metadata.width : metadata.height,
+    };
+
+    return result;
+  };
+
+export default ImageAnalyzer;
