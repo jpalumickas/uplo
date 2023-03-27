80165ff1ccd44287c77e8730e72ee31150caaf3e
Justas Palumickas <jpalumickas@gmail.com>
2023-03-28 01:32:18 +0300

    Update types

--- packages/react-native/src/getFileInfo/index.ts
+++ packages/react-native/src/getFileInfo/index.ts
@@ -1,18 +1,23 @@
-import EFS from 'expo-file-system';
-
 interface FileInfo {
   md5: string;
   size: number;
 }
 
+type EFSResult = {
+  md5?: string
+  size: number
+}
+
 const getFileInfo = async (filePath: string): Promise<FileInfo | null> => {
   try {
-    const expoFileSystem: typeof EFS = require('expo-file-system');
-    const result = await expoFileSystem.getInfoAsync(filePath, {
+    const expoFileSystem = require('expo-file-system');
+    const result: EFSResult = await expoFileSystem.getInfoAsync(filePath, {
       md5: true,
       size: true,
     });
-    if (!result || !result.md5) return null;
+
+    if (!result) return null;
+    if (!result.md5) return null;
 
     return {
       md5: result.md5,
