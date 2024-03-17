e69973bbe19418ad6b796ddbc9edbc4ec0798a7a
Justas Palumickas <jpalumickas@gmail.com>
2024-03-17 22:13:52 +0200

    Upgrade packages

---
+++ examples/cloudflare-worker/src/test.ts
@@ -0,0 +1,16 @@
+import Uplo from '@uplo/node'
+import S3Service from '@uplo/service-s3'
+
+		const uplo = Uplo({
+			config: {},
+			services: {
+				s3: S3Service({})
+			},
+			attachments: {
+				user: {
+					avatar: true
+				}
+			}
+		})
+
+console.log(uplo.attachments.user(1).avatar)
