c9c830c98ddbf779338917c400c057bc59a29e5a
Justas Palumickas <jpalumickas@gmail.com>
2024-09-17 23:30:40 +0300

    Upgrade docs next

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,37 +1,39 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 const themeConfig = {
   project: {
     link: 'https://github.com/jpalumickas/uplo',
   },
-  chat: false,
   docsRepositoryBase: 'https://github.com/jpalumickas/uplo/tree/main/docs',
-  discord: false,
-  branch: 'main', // branch of docs
-  path: '/docs', // path of docs
-  titleSuffix: ' – Uplo',
   darkMode: true,
   footer: {
     component: null,
   },
   nextThemes: {
-    defaultTheme: "light",
+    defaultTheme: 'light',
   },
-  logo: <div style={{ display: 'flex', alignItems: 'center' }}>
-    <span style={{ fontWeight: 'bold', fontSize: '1.5rem', marginRight: 20 }}>Uplo</span>
-    <span style={{ marginTop: '4px' }}>Modular uploader for Node.js</span>
-  </div>,
-  head: <>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </>,
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <span style={{ fontWeight: 'bold', fontSize: '1.5rem', marginRight: 20 }}>
+        Uplo
+      </span>
+      <span style={{ marginTop: '4px' }}>Modular uploader for Node.js</span>
+    </div>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </>
+  ),
   useNextSeoProps() {
-    const { route } = useRouter()
+    const { route } = useRouter();
 
     return {
-      description: 'Uplo can Handle file uploads to different storage services like Amazon S3, Google Cloud or etc. It also supports different type of ORM adapters, like Prisma.',
+      description:
+        'Uplo can Handle file uploads to different storage services like Amazon S3, Google Cloud or etc. It also supports different type of ORM adapters, like Prisma.',
       titleTemplate: route === '/' ? 'Uplo' : '%s - Uplo',
-    }
+    };
   },
-}
+};
 
 export default themeConfig;
